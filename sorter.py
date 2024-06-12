c4692596d81babe851bdbfb1a7258b880774ec89
PfMartin <martinpfatrisch@gmail.com>
2024-06-12 22:01:29 +0200

    Added black formatter to vs code setup

--- sorter.py
+++ sorter.py
@@ -1,15 +1,15 @@
 class Sorter:
-  def swap_elements(self, list: list, first: int, second: int) -> None:
-    (list[first], list[second]) = (list[second], list[first])
+    def swap_elements(self, list: list, first: int, second: int) -> None:
+        (list[first], list[second]) = (list[second], list[first])
 
-  def selection_sort(self, list: list) -> list:
-    for current_idx in range(len(list)):
-      min_idx = current_idx
+    def selection_sort(self, list: list) -> list:
+        for current_idx in range(len(list)):
+            min_idx = current_idx
 
-      for i in range(current_idx + 1, len(list)):
-        if list[i] < list[min_idx]:
-          min_idx = i
+            for i in range(current_idx + 1, len(list)):
+                if list[i] < list[min_idx]:
+                    min_idx = i
 
-      self.swap_elements(list, current_idx, min_idx)
-      
-    return list
+            self.swap_elements(list, current_idx, min_idx)
+
+        return list
